bb51e8d0c4155a179a370c4d6b020515b159e5fe
raiddto <raiddto@gmail.com>
2023-03-23 10:28:41 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,373 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Ryan Abcede</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+	<link rel="manifest" href="images/favicon/site.webmanifest">
+
+	<link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<nav id="colorlib-main-nav" role="navigation">
+		<div class="row">
+			<div class="col-md-12">
+				<ul>
+					<li><a href="#colorlib-about">About</a></li>
+					<li><a href="#services">Services</a></li>
+					<li><a href="#work">Works</a></li>
+					<li><a href="#testimonials">Testimonials</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	<div id="colorlib-page">
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="colorlib-navbar-brand">
+							<a class="colorlib-logo" href="index.html"><span>rai</span></a>
+						</div>
+						<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+					</div>
+				</div>
+			</div>
+		</header>
+		<div id="colorlib-about">
+			<div class="container">
+				<div class="row text-center">
+					<h2 class="bold">About</h2>
+				</div>
+				<div class="row">
+					<div class="col-md-5 animate-box">
+						<div>
+							<div class="item">
+								<img class="img-responsive about-img" src="images/about.jpg" alt="html5 bootstrap template by colorlib.com">
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-md-push-1 animate-box">
+						<div class="about-desc">
+							<div>
+								<div class="item">
+									<h2><span>Ryan Abcede</span></h2>
+								</div>
+							</div>
+							<div class="desc">
+								<div class="rotate">
+									<h2 class="heading">About</h2>
+								</div>
+								<p>I'm a programmer from the <a href="https://www.facebook.com/peo1968/">Provincial Engineering Office</a>. I've been developing programs and websites for some time now, and I find it to be a really fulfilling and exciting career. I enjoy solving complex problems and creating user-friendly interfaces that people can use easily. I'm also skilled in photo and video editing, which allows me to explore my creative side and bring out the best in every project. Overall, I'm always striving to improve my skills and produce high-quality work that my clients and colleagues will be proud of.</p>
+								<p class="colorlib-social-icons">
+									<a href="https://www.linkedin.com/in/ryanabcede/"><i class="icon-linkedin3"></i></a>
+									<a href="https://www.facebook.com/raiddto/"><i class="icon-facebook3"></i></a>
+									<a href="https://www.instagram.com/raidadto/"><i class="icon-instagram2"></i></a>
+								</p>
+								<p><a href="#" class="btn btn-primary btn-outline">Contact Me!</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="colorlib-services">
+			<div class="container">
+				<div class="row text-center">
+					<h2 class="bold">Services</h2>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="services-flex">
+							<div class="one-third">
+								<div class="row">
+									<div class="col-md-12 col-md-offset-0 animate-box intro-heading">
+										<span>My Services</span>
+										<h2>Here Are Some of My Skills</h2>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-12">
+										<div class="rotate">
+											<h2 class="heading">Services</h2>
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="services animate-box">
+											<h3>Front End Development</h3>
+											<ul>
+												<li>HTML / CSS</li>
+												<li>WordPress</li>
+												<li>Joomla</li>
+											</ul>
+										</div>
+										<div class="services animate-box">
+											<h3>Back End Development</h3>
+											<ul>
+												<li>PHP</li>
+												<li>Javascript</li>
+												<li>Python</li>
+												<li>SQL</li>
+												<li>Git</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="services animate-box">
+											<h3>Graphic Design</h3>
+											<ul>
+												<li>UI Design</li>
+												<li>Website &amp; Digital Design</li>
+												<li>Brading &amp; Visual Identity</li>
+											</ul>
+										</div>
+										<div class="services animate-box">
+											<h3>Illustration</h3>
+											<ul>
+												<li>Motion Graphics</li>
+												<li>Animation</li>
+												<li>Visual Effects</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="one-forth services-img" style="background-image: url(images/services-img-1.jpg);">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="colorlib-work">
+			<div class="container">
+				<div class="row text-center">
+					<h2 class="bold">Works</h2>
+				</div>
+				<div class="row">
+					<div class="col-md-12 col-md-offset-0 text-center animate-box intro-heading">
+						<span>Portfolio</span>
+						<h2>Done Projects</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="rotate">
+							<h2 class="heading">Portfolio</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="work-entry animate-box">
+							<a href="work.html" class="work-img" style="background-image: url(images/work-1.jpg);">
+								<div class="display-t">
+									<div class="work-name">
+										<h2>Pursuing Best</h2>
+									</div>
+								</div>
+							</a>
+							<div class="col-md-4 col-md-offset-4">
+								<div class="desc">
+									<p>Compass Booking is an innovative website that provides users with a seamless experience in booking and reserving ski cabins and equipment online. With easy registration and subscription processes, users can quickly access a wide range of available cabins and equipment and make reservations in just a few clicks. The platform is designed to be user-friendly, making it easy for both novice and experienced skiers to find the right cabin and equipment to suit their needs. Overall, Compass Booking is a one-stop-shop for all your skiing needs, offering a convenient and hassle-free way to plan your next winter adventure.</p>
+									<p class="read"><a href="#">View details</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="work-entry animate-box">
+							<a href="work.html" class="work-img" style="background-image: url(images/work-2.jpg);">
+								<div class="display-t">
+									<div class="work-name">
+										<h2>Hospice</h2>
+									</div>
+								</div>
+							</a>
+							<div class="col-md-4 col-md-offset-4">
+								<div class="desc">
+									<p>A hospice website provides valuable information and resources for individuals and families dealing with serious illness or end-of-life care. The website typically offers information about hospice services, support programs, and resources for both patients and caregivers. The website may also include tools to help patients and families navigate the hospice care process, such as directories of local hospice providers, FAQs, and online forums for support and advice. In addition to information and resources, many hospice websites offer ways to donate or volunteer to support their mission of providing compassionate care to those in need. Overall, a hospice website serves as a valuable resource for those facing difficult end-of-life decisions and provides a supportive and caring community for individuals and families during this challenging time.</p>
+									<p class="read"><a href="#">View Details</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="work-entry animate-box">
+							<a href="work.html" class="work-img" style="background-image: url(images/work-3.jpg);">
+								<div class="display-t">
+									<div class="work-name">
+										<h2>Travel and Tours</h2>
+									</div>
+								</div>
+							</a>
+							<div class="col-md-4 col-md-offset-4">
+								<div class="desc">
+									<p>A travel and tour website is a platform that provides users with comprehensive travel packages that include van transportation and entrance fees to various tourist destinations. These packages are designed to provide users with a seamless and hassle-free travel experience by taking care of all transportation and entrance fees associated with popular tourist destinations. Users can easily browse through different tour packages that cater to different interests, budgets, and group sizes. The website may also offer additional services such as hotel accommodations, meals, and guided tours. Overall, a travel and tour website is a one-stop-shop for all your travel needs, offering a convenient and cost-effective way to plan your next adventure.</p>
+									<p class="read"><a href="#">View details</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="work-entry animate-box">
+							<a href="work.html" class="work-img" style="background-image: url(images/work-4.jpg);">
+								<div class="display-t">
+									<div class="work-name">
+										<h2>Black</h2>
+									</div>
+								</div>
+							</a>
+							<div class="col-md-4 col-md-offset-4">
+								<div class="desc">
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+									<p class="read"><a href="#">View details</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="colorlib-testimony">
+			<div class="container">
+				<div class="row text-center">
+					<h2 class="bold">Testimonies</h2>
+				</div>
+				<div class="row">
+					<div class="col-md-12 col-md-offset-0 text-center animate-box intro-heading">
+						<span>Testimonies</span>
+						<h2>Clients Says</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="rotate">
+							<h2 class="heading">Says</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row animate-box">
+					<div class="owl-carousel">
+						<div class="item">
+							<div class="col-md-12 text-center">
+								<div class="testimony">
+									<blockquote>
+										<p>"A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+										<span>" &mdash; George Brooks</span>
+									</blockquote>
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="col-md-12 text-center">
+								<div class="testimony">
+									<blockquote>
+										<p>"Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+										<span>" &mdash; Daniel Foster</span>
+									</blockquote>
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="col-md-12 text-center">
+								<div class="testimony">
+									<blockquote>
+										<p>"When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove</p>
+										<span>" &mdash; Liam Jenkins</span>
+									</blockquote>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<footer>
+			<div id="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<p>
+								&copy; <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Owl Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
