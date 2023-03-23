ecfa8e113e425c503428f5604ca1781298482b29
raiddto <raiddto@gmail.com>
2023-03-23 13:40:52 +0800

    update

--- index.php
+++ index.php
@@ -53,20 +53,6 @@
 
 	</head>
 	<body>
-
-	<nav id="colorlib-main-nav" role="navigation">
-		<div class="row">
-			<div class="col-md-12">
-				<ul>
-					<li><a href="#colorlib-about">About</a></li>
-					<li><a href="#services">Services</a></li>
-					<li><a href="#work">Works</a></li>
-					<li><a href="#testimonials">Testimonials</a></li>
-					<li><a href="#contact">Contact</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
 	
 	<div id="colorlib-page">
 		<header>
@@ -74,9 +60,15 @@
 				<div class="row">
 					<div class="col-md-12">
 						<div class="colorlib-navbar-brand">
-							<a class="colorlib-logo" href="index.html"><span>rai</span></a>
+							<a class="colorlib-logo" href="/"><span>rai</span></a>
 						</div>
-						<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+						<div class="topnav">
+							<a href="/">About</a>
+							<a href="#colorlib-services">Services</a>
+							<a href="#colorlib-work">Works</a>
+							<a href="#colorlib-testimony">Testimonials</a>
+							<a href="#colorlib-contact">Contact</a>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -208,68 +200,80 @@
 				<div class="row">
 					<div class="col-md-12">
 						<div class="work-entry animate-box">
-							<a href="work.html" class="work-img" style="background-image: url(images/work-1.jpg);">
+							<a href="#" class="work-img" style="background-image: url(images/work-1.jpg);">
 								<div class="display-t">
 									<div class="work-name">
-										<h2>Pursuing Best</h2>
+										<h2>Compass Booking</h2>
 									</div>
 								</div>
 							</a>
-							<div class="col-md-4 col-md-offset-4">
+							<div class="col-md">
 								<div class="desc">
 									<p>Compass Booking is an innovative website that provides users with a seamless experience in booking and reserving ski cabins and equipment online. With easy registration and subscription processes, users can quickly access a wide range of available cabins and equipment and make reservations in just a few clicks. The platform is designed to be user-friendly, making it easy for both novice and experienced skiers to find the right cabin and equipment to suit their needs. Overall, Compass Booking is a one-stop-shop for all your skiing needs, offering a convenient and hassle-free way to plan your next winter adventure.</p>
-									<p class="read"><a href="#">View details</a></p>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div class="col-md-12">
 						<div class="work-entry animate-box">
-							<a href="work.html" class="work-img" style="background-image: url(images/work-2.jpg);">
+							<a href="#" class="work-img" style="background-image: url(images/work-2.jpg);">
 								<div class="display-t">
 									<div class="work-name">
-										<h2>Hospice</h2>
+										<h2>Hospital Hospice CC</h2>
 									</div>
 								</div>
 							</a>
-							<div class="col-md-4 col-md-offset-4">
+							<div class="col-md">
 								<div class="desc">
 									<p>A hospice website provides valuable information and resources for individuals and families dealing with serious illness or end-of-life care. The website typically offers information about hospice services, support programs, and resources for both patients and caregivers. The website may also include tools to help patients and families navigate the hospice care process, such as directories of local hospice providers, FAQs, and online forums for support and advice. In addition to information and resources, many hospice websites offer ways to donate or volunteer to support their mission of providing compassionate care to those in need. Overall, a hospice website serves as a valuable resource for those facing difficult end-of-life decisions and provides a supportive and caring community for individuals and families during this challenging time.</p>
-									<p class="read"><a href="#">View Details</a></p>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div class="col-md-12">
 						<div class="work-entry animate-box">
-							<a href="work.html" class="work-img" style="background-image: url(images/work-3.jpg);">
+							<a href="#" class="work-img" style="background-image: url(images/work-3.jpg);">
 								<div class="display-t">
 									<div class="work-name">
-										<h2>Travel and Tours</h2>
+										<h2>Unahon Travel and Tours</h2>
 									</div>
 								</div>
 							</a>
-							<div class="col-md-4 col-md-offset-4">
+							<div class="col-md">
 								<div class="desc">
 									<p>A travel and tour website is a platform that provides users with comprehensive travel packages that include van transportation and entrance fees to various tourist destinations. These packages are designed to provide users with a seamless and hassle-free travel experience by taking care of all transportation and entrance fees associated with popular tourist destinations. Users can easily browse through different tour packages that cater to different interests, budgets, and group sizes. The website may also offer additional services such as hotel accommodations, meals, and guided tours. Overall, a travel and tour website is a one-stop-shop for all your travel needs, offering a convenient and cost-effective way to plan your next adventure.</p>
-									<p class="read"><a href="#">View details</a></p>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div class="col-md-12">
 						<div class="work-entry animate-box">
-							<a href="work.html" class="work-img" style="background-image: url(images/work-4.jpg);">
+							<a href="#" class="work-img" style="background-image: url(images/work-4.jpg);">
 								<div class="display-t">
 									<div class="work-name">
-										<h2>Black</h2>
+										<h2>PhIIRE</h2>
 									</div>
 								</div>
 							</a>
-							<div class="col-md-4 col-md-offset-4">
+							<div class="col-md">
 								<div class="desc">
-									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-									<p class="read"><a href="#">View details</a></p>
+									<p>Document Identification and Tracking System (DoITS) for the Provincial Engineering Office (PEO). DoITS is a powerful document tracking system that I designed from the ground up, which includes a database and many advanced features. One of the key additions I made to the system was the integration of Geographic Information System (GIS) technology, which provides a transparent and user-friendly way for users to view details of PEO projects. With GIS, users can easily browse a map of the area and click on specific projects to view all relevant information. This functionality is similar to Google Maps, and it greatly enhances the user experience and overall effectiveness of the DoITS system.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="work-entry animate-box">
+							<a href="#" class="work-img" style="background-image: url(images/work-5.jpg);">
+								<div class="display-t">
+									<div class="work-name">
+										<h2>DOITS</h2>
+									</div>
+								</div>
+							</a>
+							<div class="col-md">
+								<div class="desc">
+									<p>Document Identification and Tracking System (DoITS) for the Provincial Engineering Office (PEO). DoITS is a powerful document tracking system that I designed from the ground up, which includes a database and many advanced features. One of the key additions I made to the system was the integration of Geographic Information System (GIS) technology, which provides a transparent and user-friendly way for users to view details of PEO projects. With GIS, users can easily browse a map of the area and click on specific projects to view all relevant information. This functionality is similar to Google Maps, and it greatly enhances the user experience and overall effectiveness of the DoITS system.</p>
 								</div>
 							</div>
 						</div>
@@ -302,8 +306,18 @@
 							<div class="col-md-12 text-center">
 								<div class="testimony">
 									<blockquote>
-										<p>"A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-										<span>" &mdash; George Brooks</span>
+										<p>"Your programming skills are truly impressive! Your ability to write clean, efficient code is a testament to your dedication and expertise in the field. Your programs always work flawlessly and you consistently exceed expectations. It's a pleasure to work with someone who is so talented and committed to their craft. Keep up the great work!</p>
+										<span>" &mdash; Anna Faye Villaflor</span>
+									</blockquote>
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="col-md-12 text-center">
+								<div class="testimony">
+									<blockquote>
+										<p>"I recently used this compass booking website and I must say, I was thoroughly impressed with the ease of use and the range of options available. The booking process was seamless and the site provided detailed information about the ski cabins and equipment available for rent. The prices were also very reasonable, making it easy for me to plan a budget-friendly ski trip. Overall, I highly recommend this website to anyone looking to book a ski trip, whether you're a seasoned pro or a first-time skier.</p>
+										<span>" &mdash; Jeremy Roy</span>
 									</blockquote>
 								</div>
 							</div>
@@ -313,7 +327,7 @@
 								<div class="testimony">
 									<blockquote>
 										<p>"Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
-										<span>" &mdash; Daniel Foster</span>
+										<span>" &mdash; Barth Unahon</span>
 									</blockquote>
 								</div>
 							</div>
@@ -323,8 +337,66 @@
 								<div class="testimony">
 									<blockquote>
 										<p>"When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove</p>
-										<span>" &mdash; Liam Jenkins</span>
+										<span>" &mdash; Engr. Mario Batar</span>
 									</blockquote>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div id="colorlib-contact">
+			<div class="container">
+				<div class="row text-center">
+					<h2 class="bold">Contact</h2>
+				</div>
+				<div class="row">
+					<div class="col-md-12 col-md-offset-0 text-center animate-box intro-heading">
+						<span>Contact</span>
+						<h2>Contact Me</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="rotate">
+							<h2 class="heading">Contact</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 col-md-offset-0">
+						<div class="row">
+							<div class="col-md-4 animate-box">
+								<h3>My Address</h3>
+								<ul class="contact-info">
+									<li><span><i class="icon-map5"></i></span>Cagayan de Oro City</li>
+									<li><span><i class="icon-envelope2"></i></span><a href="#">raiabcede@gmail.com</a></li>
+								</ul>
+							</div>
+							<div class="col-md-7 col-md-push-1 animate-box">
+								<div class="row">
+									<div class="col-md-12">
+										<div class="form-group">
+											<textarea name="" class="form-control" id="" cols="30" rows="7" placeholder="Message"></textarea>
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Name">
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Email">
+										</div>
+									</div>
+									<div class="col-md-12">
+										<div class="form-group">
+											<input type="submit" value="Send Message" class="btn btn-primary">
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
--- js/main.js
+++ js/main.js
@@ -26,23 +26,6 @@
 
 	};
 
-	var burgerMenu = function() {
-
-		$('.js-colorlib-nav-toggle').on('click', function(event) {
-			event.preventDefault();
-			var $this = $(this);
-			if( $('body').hasClass('menu-show') ) {
-				$('body').removeClass('menu-show');
-				$('#colorlib-main-nav > .js-colorlib-nav-toggle').removeClass('show');
-			} else {
-				$('body').addClass('menu-show');
-				setTimeout(function(){
-					$('#colorlib-main-nav > .js-colorlib-nav-toggle').addClass('show');
-				}, 900);
-			}
-		})
-	};
-
 	// Animations
 
 	var contentWayPoint = function() {
@@ -166,7 +149,6 @@
 	// Document on load.
 	$(function(){
 		fullHeight();
-		burgerMenu();
 		counterWayPoint();
 		contentWayPoint();
 		owlCarouselFeatureSlide();
